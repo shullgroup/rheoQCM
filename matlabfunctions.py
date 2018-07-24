e7a2d6298475cc8d021df7477bd1525f7f12641c
Megan Yang <megan.j.yang@gmail.com>
2018-07-24 13:55:14 -0500

    update mlf num2str

--- matlabfunctions.py
+++ matlabfunctions.py
@@ -1,9 +1,32 @@
 import math
 import numpy as np
 
-def num2str(A,precision):
-    if type(A) == np.ndarray:
-        array = A
-        for i in range(len(array)):
-            array[i] = format(array[i], '.'+str(precision)+'g')
-        return array
+def num2str(A,precision=None, formatSpec=None):
+    if isinstance(A, np.ndarray):
+        if A.any() and not precision and not formatSpec:
+            for i in range(len(A)):
+                A[i] = str(float(A[i]))
+            print(A)
+            return A
+        elif A.any() and precision and not formatSpec:
+            for i in range(len(A)):
+                A[i] = format(float(A[i]), '.'+str(precision)+'g')
+            print(A)
+            return A
+        elif A.any() and formatSpec:
+            print('not implemented')
+        else:
+            print('not available')
+    elif isinstance(A, float) or isinstance(A, int):
+        if A and not precision and not formatSpec:
+            A = str(float(A))
+            print(A)
+            return A
+        elif A and precision and not formatSpec:
+            A = format(float(A), '.'+str(precision)+'g')
+            print(A)
+            return A
+        elif A and formatSpec:
+            print('not implemented')
+        else:
+            print('not available')
