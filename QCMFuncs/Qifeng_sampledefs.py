a98ed6bc43b6890fa170c5fbfa0aafeca592aefe
krshull <k-shull@northwestern.edu>
2018-08-12 00:10:41 -0500

    add Qifeng's definitions

---
+++ QCMFuncs/Qifeng_sampledefs.py
@@ -0,0 +1,176 @@
+# note that the values of fref used for the springpot models are for the
+# reference temperature from the original DMA data, which is a bit
+# arbitrary
+
+
+def sample_dict():
+    sample = {}  # individual sample dictionaries get added to sample
+
+    #  20180502
+    samplename = 'cryt_2_BCB_air'
+    sample[samplename] = {
+    'samplename': 'cryt_2_BCB_air',
+    'datadir': 'data/Qifeng/20180502/',
+    'barefile': 'cryt_2_bare_air',
+    'filmfile': 'cryt_2_BCB_air',
+    # 'firstline': 50,
+    # 'filmtrange': [4000, 5000],
+    'nhcalc': ['355', '353'],
+    'nhplot': [1, 3, 5]
+    }
+
+    samplename = 'cryt_2_BCB_LN2'
+    sample[samplename] = {
+    'samplename': 'cryt_2_BCB_LN2',
+    'datadir': 'data/Qifeng/20180502/',
+    'barefile': 'cryt_2_bare_LN2',
+    'filmfile': 'cryt_2_BCB_LN2',
+    # 'firstline': 50,
+    # 'filmtrange': [4000, 5000],
+    'nhcalc': ['355', '353'],
+    'nhplot': [1, 3, 5]
+    }
+
+    samplename = 'cryt_2_BCB_air_after_LN2'
+    sample[samplename] = {
+    'samplename': 'cryt_2_BCB_air_after_LN2',
+    'datadir': 'data/Qifeng/20180502/',
+    'barefile': 'cryt_2_bare_air',
+    'filmfile': 'DGEBA-cryt_2_BCB_air_after_LN2',
+    # 'firstline': 50,
+    # 'filmtrange': [4000, 5000],
+    'nhcalc': ['355', '353'],
+    'nhplot': [1, 3, 5]
+    }
+
+    #  20180629
+    samplename = 'DGEBA-Jeffamine2000_RT'
+    sample[samplename] = {
+    'samplename': 'DGEBA-Jeffamine2000_RT',
+    'datadir': 'data/Qifeng/20180629/',
+    'barefile': 'bare_air',
+    'filmfile': 'DGEBA-Jeffamine2000_RT',
+    'firstline': 50,
+    # 'filmtrange': [4000, 5000],
+    'nhcalc': ['355', '353'],
+    'nhplot': [1, 3, 5]
+    }
+
+    #  20180711
+    samplename = 'DGEBA-Jeffamine230_RT'
+    sample[samplename] = {
+    'samplename': 'DGEBA-Jeffamine230_RT',
+    'datadir': 'data/Qifeng/20180711/',
+    'barefile': 'bare_air',
+    'filmfile': 'DGEBA-Jeffamine230_RT',
+    'firstline': 1,
+    # 'filmtrange': [1, 10],
+    'nhcalc': ['355'],
+    'nhplot': [3, 5]
+    }
+
+    #  20180713
+    samplename = 'DGEBA-Jeffamine230_RT_2'
+    sample[samplename] = {
+    'samplename': 'DGEBA-Jeffamine230_RT_2',
+    'datadir': 'data/Qifeng/20180713/',
+    'barefile': 'bare_air',
+    'filmfile': 'DGEBA-Jeffamine230_RT_2',
+    'firstline': 1,
+    # 'filmtrange': [1, 10],
+    'nhcalc': ['355'],
+    'nhplot': [3, 5]
+    }
+
+    #  20180724 1:1 Good
+    samplename = 'DGEBA-Jeffamine230_RT_3'
+    sample[samplename] = {
+    'samplename': 'DGEBA-Jeffamine230_RT_3',
+    'datadir': 'data/Qifeng/20180724/',
+    'barefile': 'bare_air',
+    'filmfile': 'DGEBA-Jeffamine230_RT_3',
+    'firstline': 1,
+    # 'filmtrange': [1, 10],
+    'nhcalc': ['355'],
+    'nhplot': [3, 5]
+    }
+
+    #  20180726 2:1 too thick
+    samplename = 'DGEBA-Jeffamine230_RT_4'
+    sample[samplename] = {
+    'samplename': 'DGEBA-Jeffamine230_RT_4',
+    'datadir': 'data/Qifeng/20180726/',
+    'barefile': 'bare_air',
+    'filmfile': 'DGEBA-Jeffamine230_RT_4',
+    'firstline': 2,
+    # 'filmtrange': [1, 10],
+    'nhcalc': ['355'],
+    'nhplot': [3, 5]
+    }
+
+    #  20180727 2:1 Good
+    samplename = 'DGEBA-Jeffamine230_RT_5'
+    sample[samplename] = {
+    'samplename': 'DGEBA-Jeffamine230_RT_5',
+    'datadir': 'data/Qifeng/20180727/',
+    'barefile': 'bare_air',
+    'filmfile': 'DGEBA-Jeffamine230_RT_5',
+    'firstline': 2,
+    'filmtrange': [500, 50000],
+    'nhcalc': ['355'],
+    'nhplot': [3, 5]
+    }
+
+    #  20180803 2:1 
+    samplename = 'DGEBA-Jeffamine2000_RT_2'
+    sample[samplename] = {
+    'samplename': 'DGEBA-Jeffamine2000_RT_2',
+    'datadir': 'data/Qifeng/20180803/',
+    'barefile': 'bare_air',
+    'filmfile': 'DGEBA-Jeffamine2000_RT_2',
+    'firstline': 1,
+    # 'filmtrange': [1, 10],
+    'nhcalc': ['355'],
+    'nhplot': [1, 3, 5]
+    }
+
+    #  20180806 2:1 
+    samplename = 'DGEBA-Jeffamine2000_RT_3'
+    sample[samplename] = {
+    'samplename': 'DGEBA-Jeffamine2000_RT_3',
+    'datadir': 'data/Qifeng/20180806/',
+    'barefile': 'bare_air',
+    'filmfile': 'DGEBA-Jeffamine2000_RT_3',
+    'firstline': 1,
+    # 'filmtrange': [1, 10],
+    'nhcalc': ['355', '353'],
+    'nhplot': [1, 3, 5]
+    }
+
+    #  20180807 2:1 
+    samplename = 'DGEBA-Jeffamine2000_RT_3_2'
+    sample[samplename] = {
+    'samplename': 'DGEBA-Jeffamine2000_RT_3_2',
+    'datadir': 'data/Qifeng/20180807/',
+    'barefile': 'bare_air',
+    'filmfile': 'DGEBA-Jeffamine2000_RT_3_2',
+    'firstline': 1,
+    # 'filmtrange': [1, 10],
+    'nhcalc': ['355', '353'],
+    'nhplot': [1, 3, 5]
+    }
+
+    #  20180808 2:1 
+    samplename = 'DGEBA-Jeffamine2000_RT_4'
+    sample[samplename] = {
+    'samplename': 'DGEBA-Jeffamine2000_RT_4',
+    'datadir': 'data/Qifeng/20180808/',
+    'barefile': 'bare_air',
+    'filmfile': 'DGEBA-Jeffamine2000_RT_4',
+    'firstline': 1,
+    # 'filmtrange': [1, 10],
+    'nhcalc': ['355', '353'],
+    'nhplot': [1, 3, 5]
+    }
+
+    return sample
---
+++ QCMFuncs/Qigeng_runfile.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  8 01:23:49 2018
+
+@author: ken
+
+"""
+
+# %%
+import matplotlib.pyplot as plt
+from Qifeng_sampledefs import sample_dict
+from QCM_functions import QCManalyze
+parms = {}  # parameters to pass to QCManalyze
+sample = sample_dict()  # read sample dictionary
+
+# specify any non-default parameters
+parms['imagetype'] = 'pdf'  # default is 'svg'
+#%%
+QCManalyze(sample['cryt_2_BCB_air'], parms)
+#%%
+QCManalyze(sample['cryt_2_BCB_LN2'], parms)
+#%%
+QCManalyze(sample['cryt_2_BCB_air_after_LN2'], parms)
+
+#%%
+QCManalyze(sample['DGEBA-Jeffamine2000_RT'], parms)
+
+#%%
+QCManalyze(sample['DGEBA-Jeffamine230_RT'], parms)
+
+#%%
+QCManalyze(sample['DGEBA-Jeffamine230_RT_2'], parms)
+jj
+#%% 1:1 good
+QCManalyze(sample['DGEBA-Jeffamine230_RT_3'], parms)
+j
+#%% 2:1 thick
+QCManalyze(sample['DGEBA-Jeffamine230_RT_4'], parms)
+
+#%% 2:1 good
+QCManalyze(sample['DGEBA-Jeffamine230_RT_5'], parms)
+
+#%% 2:1 
+QCManalyze(sample['DGEBA-Jeffamine2000_RT_2'], parms)
+
+#%% 2:1 
+QCManalyze(sample['DGEBA-Jeffamine2000_RT_3'], parms)
+
+#%% 2:1 
+QCManalyze(sample['DGEBA-Jeffamine2000_RT_4'], parms)
