8611cdceddbd43841d4fa791b9c978eb36110b15
zhczq <qifeng.wang@u.northwestern.edu>
2018-08-20 18:40:50 -0500

    UI curve fitting initiated

---
+++ modules/GBFitting.py
@@ -0,0 +1,13 @@
+'''
+class for fitting of G and B
+'''
+from scipy.optimize import leastsq
+import lmfit
+
+class GBFitting:
+    def __init__(self, f, G, B):
+        pass
+
+
+if __name__ == 'main':
+    gbfitting = GBFitting()
