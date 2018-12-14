586f8d8f0e88deb60a237b1fb2d383c4dc141a5b
zhczq <qifeng.wang@u.northwestern.edu>
2018-12-13 22:34:56 -0600

    ver

--- _version.py
+++ _version.py
@@ -1,4 +1,4 @@
-__version_info__ = ('0', '1', '15') # major, minor, micro
+__version_info__ = ('0', '2', '0') # major, minor, micro
 
 __projectname__ = 'QCM Rheology'
 __author__ = 'Qifeng Wang'
@@ -7,5 +7,5 @@ __contact__ = ''
 __copyright__ = 'Shull Research Group @ Northwestern University'
 __license__ = 'GLP-3.0'
 __deprecated__ = ''
-__date__ = '6 Dec. 2018'
+__date__ = '13 Dec. 2018'
 __version__ = '.'.join(__version_info__)
