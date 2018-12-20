408aadeb1000066eaee4a14477588b8e1162545c
zhczq <qifeng.wang@u.northwestern.edu>
2018-12-20 16:16:50 -0600

    ver

--- QCM_main/_version.py
+++ QCM_main/_version.py
@@ -1,4 +1,4 @@
-__version_info__ = ('0', '5', '1') # major, minor, micro
+__version_info__ = ('0', '6', '0') # major, minor, micro
 
 __projectname__ = 'QCM Rheology'
 __authors__ = ['Qifeng Wang', 'Megan Yang', 'Kenneth R. Shull']
