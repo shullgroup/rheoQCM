f9a5016100403ca88f0676bfccfe8e2a0afdc5c8
zhczq <qifeng.wang@u.northwestern.edu>
2019-01-04 15:29:41 -0600

    fix bug of data/settings with temp inforamtion chrashing on computer w/o temp sensor

--- tests/modules/TempDevices.py
+++ tests/modules/TempDevices.py
@@ -50,6 +50,9 @@ def device_info(devtype):
     print('devtype', devtype) #testprint
     devices = list_devices()
     for device in devices:
-        if device.product_type != devtype:
-            devices.remove(device)
-    return devices[0]
+    #     if device.product_type != devtype:
+    #         devices.remove(device)
+    # return devices[0]
+        if device.product_type == devtype:
+            return device
+    return None
