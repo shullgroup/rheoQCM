fix reference calculation bugs.

tests/modules/DataSaver.py
@@ -398,7 +398,7 @@ class DataSaver:
 
             # calculate func which cannot be saved in file
             for chn_name in self._chn_keys:
-                self.calc_fg_ref(chn_name, mark=True)
+                self.calc_fg_ref(chn_name, mark=False) # False or True??
 
             self.raw  = {} # raw data from last queue
 
@@ -669,22 +669,32 @@ class DataSaver:
         return a set of raw data (f, G, B) or (f, G, B, t, temp)
         '''
         with h5py.File(self.path, 'r') as fh:
-            t = fh['raw/' + chn_name + '/' + str(queue_id)].attrs['t']
-            if 'temp' in fh['raw/' + chn_name + '/' + str(queue_id)].attrs.keys():
-                temp = fh['raw/' + chn_name + '/' + str(queue_id)].attrs['temp']
+            t = fh['raw/' + chn_name + '/' + str(int(queue_id))].attrs['t']
+            if 'temp' in fh['raw/' + chn_name + '/' + str(int(queue_id))].attrs.keys():
+                temp = fh['raw/' + chn_name + '/' + str(int(queue_id))].attrs['temp']
             else:
                 temp = np.nan
 
             if self._raw_exists(fh, chn_name, queue_id, harm): # raw data exist
-                self.raw = fh['raw/' + chn_name + '/' + str(queue_id) + '/' + harm][()]
+                raw = fh['raw/' + chn_name + '/' + str(int(queue_id)) + '/' + harm][()]
+                self.raw = {
+                    'f': raw[0, :],
+                    'G': raw[1, :],
+                    'B': raw[2, :],
+                }
+        
             else: # raw data doesn't exist
-                self.raw = np.array([[np.nan], [np.nan], [np.nan]])
                 logger.warning('No raw data found for %s, %s, %s', chn_name, queue_id, harm)
-        logger.info(type(self.raw)) 
+                self.raw = {
+                    'f': None,
+                    'G': None,
+                    'B': None,
+                }
+
         if with_t_temp:
-            return [self.raw[0, :], self.raw[1, :], self.raw[2, :], t, temp]
+            return [self.raw['f'], self.raw['G'], self.raw['B'], t, temp]
         else: 
-            return [self.raw[0, :], self.raw[1, :], self.raw[2, :]]
+            return [self.raw['f'], self.raw['G'], self.raw['B']]
 
 
     def _raw_exists(self, file_handle, chn_name, queue_id, harm):
@@ -692,9 +702,17 @@ class DataSaver:
         check if corresponding raw data exists
         file_handle: handle to the file
         '''
-        if 'raw' in file_handle.keys() and chn_name in file_handle['raw'] and str(queue_id) in file_handle['raw/'+chn_name] and harm in file_handle['raw/' + chn_name + '/' + str(queue_id)]: 
+        logger.info('raw chn:%s id:%s harm:%s', chn_name, queue_id, harm)
+        logger.info('raw in fh: %s', 'raw' in file_handle.keys())
+        logger.info('chn_name in fh["raw"]: %s', chn_name in file_handle['raw'])
+        logger.info('queue_id in raw/chn_name: %s', str(int(queue_id)) in file_handle['raw/'+chn_name])
+        logger.info('harm in raw/chn_name/queue_id: %s', harm in file_handle['raw/' + chn_name + '/' + str(int(queue_id))])
+
+        if 'raw' in file_handle.keys() and chn_name in file_handle['raw'] and str(int(queue_id)) in file_handle['raw/'+chn_name] and harm in file_handle['raw/' + chn_name + '/' + str(int(queue_id))]: 
+            logger.info('raw exists')
             return True
         else:
+            logger.info('raw does not exists')
             return False
 
 
@@ -885,7 +903,7 @@ class DataSaver:
                 self.reshape_data_df('ref', mark=mark, dropnanmarkrow=dropnanmarkrow, dropnancolumn=dropnancolumn, deltaval=True, unit_t=unit_t, unit_temp=unit_temp, keep_mark=True), # keep only one marks
                 on=['queue_id', 't', 'temp']
             )
-
+        if self.ref_ref.shape[0] > 0:
             df_ref_ref = self.reshape_data_df('ref_ref', mark=mark, dropnanmarkrow=dropnanmarkrow, dropnancolumn=dropnancolumn, unit_t=unit_t, unit_temp=unit_temp)
 
          # get ext
@@ -1617,12 +1635,12 @@ class DataSaver:
                 # samp as sample and ref as reference
                 # use fitting of reference  
 
-                # clear self.samp_ref
-                if self.samp_ref.shape[0] > 0: 
-                    self.samp_ref = self._make_df()
+                # clear self.<chn_name>_ref
+                if getattr(self, chn_name + '_ref').shape[0] > 0: 
+                    setattr(self, chn_name + '_ref', self._make_df())
                 # clear all self.exp_ref[chn_name]
-                for chn_name in self._chn_keys: 
-                    self.exp_ref[chn_name] = {
+                for chn_name_temp in self._chn_keys: 
+                    self.exp_ref[chn_name_temp] = {
                         'f0': self.nan_harm_list(), # list for each harmonic
                         'g0': self.nan_harm_list(), # list for each harmonic
                     }
@@ -1741,14 +1759,12 @@ class DataSaver:
                 self.exp_ref['func'][chn_name] = func_list # save to class
 
                 # calculate ref for each sample (samp) temp and save to self.samp_ref
-                cols_df = self.interp_film_ref('samp')
 
                 # copy df from samp_ref_source
                 df = getattr(self, chn_name).copy()
 
-                # copy film ref to df
-                df.fs = cols_df.fs
-                df.gs = cols_df.gs
+                df['fs'] = self.interp_film_ref(chn_name, col='fs')
+                df['gs'] = self.interp_film_ref(chn_name, col='gs')
 
                 # change mark 1 to 1
                 df = self.reset_match_marks(df, mark_pair=(0, 1)) # mark 1 to 0
@@ -2109,6 +2125,9 @@ class DataSaver:
                         logger.info(df_chn.queue_id[ind]) 
                         logger.info(fh['raw/' + chn_name + '/' + str(int(df_chn.queue_id[ind])) + '/' + harm]) 
                         del fh['raw/' + chn_name + '/' + str(int(df_chn.queue_id[ind])) + '/' + harm]
+                        logger.warning('raw data deleted (%s, %s, %s)', chn_name, df_chn.queue_id[ind], harm)
+                    else:
+                        logger.warning('raw data does not exist (%s, %s, %s)', chn_name, df_chn.queue_id[ind], harm)
 
 
     ######## functions for unit convertion #################

tests/rheoQCM.py
@@ -1734,8 +1734,8 @@ class QCMApp(QMainWindow):
         '''
         recalculate delf and delg by reference set saved in data_saver
         '''
-        self.data_saver.calc_fg_ref('samp', mark=True)
-        self.data_saver.calc_fg_ref('ref', mark=True)
+        self.data_saver.calc_fg_ref('samp', mark=False) # False or True??
+        self.data_saver.calc_fg_ref('ref', mark=False) # False or True??
 
 
     def on_triggered_actionOpen_MyVNA(self):
@@ -2550,6 +2550,10 @@ class QCMApp(QMainWindow):
 
     def tab_spectra_fit_update_mpls(self, f, G, B):
         ''' update mpl_spectra_fit and mpl_spectra_fit_polar '''
+        print(f)
+        if f is None or G is None or B is None:
+            logger.warning('None data.')
+            return
         ## disconnect axes event
         self.mpl_disconnect_cid(self.ui.mpl_spectra_fit)
 
@@ -2822,10 +2826,7 @@ class QCMApp(QMainWindow):
         idx = np.where((data_lG[0] >= factor_span[0]) & (data_lG[0] <= factor_span[1]))[0] # determine the indices by f (data_lG[0])
 
         logger.info('idx: %s', idx) 
-        print(type(idx))
-        print(idx.dtype)
-        print(fit_result['fit_g'])
-        print(fit_result['fit_g'][idx])
+
         self.ui.mpl_spectra_fit_polar.update_data({'ln': 'lsp', 'x': fit_result['fit_g'][idx], 'y': fit_result['fit_b'][idx]})
 
         if self.get_spectraTab_mode() == 'center': # center mode
@@ -2885,7 +2886,6 @@ class QCMApp(QMainWindow):
 
         # get data from data saver
         f, G, B = self.get_active_raw()
-        logger.info('%s, %s, %s', len(f), len(G), len(B)) 
 
         # update raw
         self.tab_spectra_fit_update_mpls(f, G, B)
@@ -3809,20 +3809,15 @@ class QCMApp(QMainWindow):
         logger.info(pk_data[0]) 
         logger.info(type(pk_data)) 
         logger.info(type(pk_data[0])) 
-        logger.info(isinstance(pk_data[0], float)) 
-        logger.info(isinstance(pk_data[0], np.float)) 
-        logger.info(isinstance(pk_data[0], np.float64)) 
-        logger.info(isinstance(pk_data[0], int)) 
-        logger.info(isinstance(pk_data[0], np.int)) 
-        logger.info(isinstance(pk_data[0], np.int32)) 
-        logger.info(isinstance(pk_data[0], np.int64)) 
+
         if isinstance(pk_data[0], (float, int, np.int64)): # data is not empty (float for values, int for index and queue_id)
             label = mpl.l['lp'][0].get_label()
             line, ind = label.split('_')
             l, harm = line[:-1], line[-1]
             logger.info('label: %s', label) 
             logger.info(line) 
-            logger.info(l, harm) 
+            logger.info(l) 
+            logger.info(harm) 
             logger.info(ind) 
 
             self.active['chn_name'] = self.get_plt_chnname(plt_str)